d6ca9d4c5940d08c571a0aa165f02f1ea7e250f6
DESKTOP-MALGT2M\krtod <bflower1030@gmail.com>
2025-11-09 02:28:27 +0900

    PostRepository

--- backend/src/main/java/app/back/code/post/repository/PostRepository.java
+++ backend/src/main/java/app/back/code/post/repository/PostRepository.java
@@ -1,4 +1,32 @@
 package app.back.code.post.repository;
 
-public interface PostRepository {
+import app.back.code.post.entity.PostEntity;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface PostRepository extends JpaRepository<PostEntity, Long>, JpaSpecificationExecutor<PostEntity> {
+
+    List<Long> findPostIdByWriter_UserId(String userId);
+
+    Page<PostEntity> findByCategory_CategoryIdInAndFileType_CategoryIdIn(
+            List<Long> categoryIds,
+            List<Long> fileTypeIds,
+            Pageable pageable
+    );
+
+    Page<PostEntity> findByCategory_CategoryIdIn(
+            List<Long> categoryIds,
+            Pageable pageable
+    );
+
+    Page<PostEntity> findByFileType_CategoryIdIn(
+            List<Long> fileTypeIds,
+            Pageable pageable
+    );
 }
